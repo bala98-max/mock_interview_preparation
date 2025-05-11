e94c77b1aaf5336427b2be121d6298c1aab44ef5
balamax98 <balamanikandan.developer@gmail.com>
2025-05-11 17:18:21 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Prepwise",
+  description: "AI Powered Mock interview preparation",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${monaSans.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
